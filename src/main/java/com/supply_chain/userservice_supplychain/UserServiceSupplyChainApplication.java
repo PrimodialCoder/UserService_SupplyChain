3a988ccaf58dc800392443b2ec483c57dc0e3b4c
Pranav  Gupta <pranav.gupta@intimetec.com>
2026-04-24 14:49:48 +0530

    Added entity listeners

--- src/main/java/com/supply_chain/userservice_supplychain/UserServiceSupplyChainApplication.java
+++ src/main/java/com/supply_chain/userservice_supplychain/UserServiceSupplyChainApplication.java
@@ -2,8 +2,10 @@ package com.supply_chain.userservice_supplychain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class UserServiceSupplyChainApplication {
 
     public static void main(String[] args) {
